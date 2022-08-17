65f7fb12f666ee11593f2542d56d246cfd35cb72
Shuthaku <thakur.shubham20@gmail.com>
2022-08-17 07:18:02 +0530

    Adding file on 17/08

---
+++ car.java
@@ -0,0 +1,16 @@
+package abstractclass1;
+
+public interface car {
+	
+	//Always define only abstract methods
+	//no method body
+	//only method declaration
+	//we achieve 100% abstraction
+	
+	 int a=10;//by default static & final
+	
+	public void start();
+	public void stop();
+	public void refuel();
+
+}
